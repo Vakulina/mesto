1e13b8291b8854e978ab62be9c14d13529ef7db4
Vakulina <viktoria.ganina@yandex.ru>
2021-08-11 15:09:42 +0300

    feat: add functional of popup-form: open? close, change name and specification

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-let openPopupButton = document.querySelector('.profile__open-popup');
 let formPopup = document.querySelector('.popup');
-//let openPopupButton = document.querySelector('.nav__button');
+let openPopupButton = document.querySelector('.profile__open-popup');
 let closePopupButton = document.querySelector('.popup__reset-button');
+let submitButton = document.querySelector('.popup__submit-button');
 
-openPopupButton.addEventListener('click', function () {
+function formOpen() {
   formPopup.classList.add('popup_opened');
-});
-closePopupButton.addEventListener('click', function () {
+}
+
+function formClose() {
   formPopup.classList.remove('popup_opened');
-});
+}
+
+openPopupButton.addEventListener('click', formOpen);
+closePopupButton.addEventListener('click', formClose);
+submitButton.addEventListener('click', formClose);
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__specification');
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+  let newSpecification = document.querySelector('.profile__specification');
+  let newName = document.querySelector('.profile__title');
+  newName.textContent = nameInput.value; 
+  newSpecification.textContent = jobInput.value;
+}
+formElement.addEventListener('submit', formSubmitHandler); 
