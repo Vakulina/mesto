bce922f363b07f87d4b85d862db2c1b7ff4590bd
Vakulina <viktoria.ganina@yandex.ru>
2021-08-31 21:39:44 +0300

    feat: add function of publication of card

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,52 @@
+const initialCards = [
+  {
+    name: 'Байкал',
+    link: './images/Baikal.jpg'
+  },
+  {
+    name: 'Москва',
+    link: './images/Moskow.jpg'
+  },
+  {
+    name: 'Мыс Фиолент',
+    link: './images/Fiolent.jpg'
+  },
+  {
+    name: 'Петергоф',
+    link: './images/Peterhof.jpg'
+  },
+  {
+    name: 'Ростов-на-Дону - столица Донского края',
+    link: './images/Rostow.jpg'
+  },
+  {
+    name: 'Санкт-Петербург',
+    link: './images/SPB.jpg'
+  }
+];
+
+const cardTemplate = document.getElementById('place-card').content;
+const likesToggle = (event) => {        //функция ставит и убирает лайки
+  event.target.classList.toggle('place__like-button_active');
+};
+
+const postingCard = (card) => {    //функция публикации карточки
+  const postingElement = cardTemplate.querySelector('.place').cloneNode(true); //определим шаблон карточки и клонируем его для публикации 
+  const likeButton = cardTemplate.querySelector('.place__like-button'); //найдем кнопку лайков
+
+  likeButton.classList.toggle('place__like-button_active');
+  const placeOfPublication = document.querySelector('.places'); //найдем место публикации карточек
+  postingElement.querySelector('.place__img').src = card.link; //определим содержимое каждого элемента карточки
+  postingElement.querySelector('.place__paragraf').textContent = card.name;
+  postingElement.querySelector('.place__img').alt = `Фото пользователя: ${card.name}`;
+  placeOfPublication.prepend(postingElement); //поместим новую карточку в документ
+  likeButton.addEventListener('click', likesToggle); //повесим слушатель на кнопку лайков   
+};
+initialCards.forEach(postingCard); //опубликуем первоначальный массив карточек
+
+
+
+
 let popup = document.querySelector('.popup');
 let formPopup = popup.querySelector('.popup__form');
 let openPopupButton = document.querySelector('.profile__open-popup');
