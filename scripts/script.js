a24e385c8a9c1004d6e2871510ddc4d03997ae3e
Vakulina <viktoria.ganina@yandex.ru>
2021-09-03 21:33:50 +0300

    feat: add html-code of popup_image

--- scripts/script.js
+++ scripts/script.js
@@ -25,50 +25,114 @@ const initialCards = [
   }
 ];
 
-const cardTemplate = document.getElementById('place-card').content;
+const cardTemplate = document.getElementById('place-card').content; //выбираем контейтер template с шаблоном карточки
 const likesToggle = (event) => {        //функция ставит и убирает лайки
    event.target.classList.toggle('place__like-button_active');
 };
-
-const postingCard = (card) => {    //функция публикации карточки
-  const postingElement = cardTemplate.querySelector('.place').cloneNode(true); //определим шаблон карточки и клонируем его для публикации 
-  const likeButton = postingElement.querySelector('.place__like-button'); //найдем кнопку лайков
-  const placeOfPublication = document.querySelector('.places'); //найдем место публикации карточек
-  
-  postingElement.querySelector('.place__img').src = card.link; //определим содержимое каждого элемента карточки
-  postingElement.querySelector('.place__paragraf').textContent = card.name;
-  postingElement.querySelector('.place__img').alt = `Фото пользователя: ${card.name}`;
-  placeOfPublication.prepend(postingElement); //поместим новую карточку в документ
-  likeButton.addEventListener('click', likesToggle ); //повесим слушатель на кнопку лайков   
-};
-initialCards.forEach(postingCard); //опубликуем первоначальный массив карточек
-
-
-
-
-let popup = document.querySelector('.popup');
-let formPopup = popup.querySelector('.popup__form');
-let openPopupButton = document.querySelector('.profile__open-popup');
-let closePopupButton = popup.querySelector('.popup__reset-button');
-let nameInput = formPopup.popup__name;
-let jobInput = formPopup.popup__specification;
-let profileSpecification = document.querySelector('.profile__specification');
-let profileName = document.querySelector('.profile__title');
-
-function formOpen() {
-  popup.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileSpecification.textContent;
+const deleteCard = (event) => {
+  event.target.closest('.place').remove();
 }
-openPopupButton.addEventListener('click', formOpen);
-function formClose() {
+const openBigImage = ()=>{
+
+}
+
+let postingElement; 
+const creatNewCard = (card) => {
+  postingElement= cardTemplate.querySelector('.place').cloneNode(true); //определим шаблон карточки и клонируем его для публикации 
+  const likeButton = postingElement.querySelector('.place__like-button'); //находим кнопку лайков
+  const buttonTrashPlace = postingElement.querySelector('.place__trash-button');
+  const imgOpening = postingElement.querySelector('.place__img');
+  postingElement.querySelector('.place__img').src = card.link; //опредяем содержимое каждого элемента карточки
+  postingElement.querySelector('.place__paragraf').textContent = card.name;
+  imgOpening.alt = `Фото пользователя: ${card.name}`;
+  likeButton.addEventListener('click', likesToggle ); //вешаем слушатель на кнопку лайков  
+  buttonTrashPlace.addEventListener('click', deleteCard);  //вешаем слушатель на кнопку с корзиной (удалить карточку)
+  imgOpening.addEventListener('click', ()=>{
+    console.log("big image");
+  });
+  return postingElement;
+}
+const postingCard = (card) => { 
+  creatNewCard(card);
+  const placeOfPublication = document.querySelector('.places'); //находим место публикации карточек
+  placeOfPublication.prepend(postingElement); //помещаем новую карточку в документ};
+};
+
+initialCards.forEach(postingCard); //публикуем первоначальный массив карточек
+
+//находим попапы 
+const profilePopup = document.querySelector('.popup_type_profile'); 
+const placePopup = document.querySelector('.popup_type_place');
+const imagePopup = document.querySelector('.popup_type_image');
+//находим кнопки
+const buttonOpenProfile = document.querySelector('.profile__open-popup');
+const buttonCloseProfile = document.querySelector('.popup__reset-button_profile');
+const buttonClosePlace = document.querySelector('.popup__reset-button_place');
+const submitProfile = profilePopup.querySelector('.popup__form_profile');
+const submitNewPlace = placePopup.querySelector('.popup__form_place');
+//находим инпуты
+let inputName = document.querySelector('.popup__input_type-name');
+let inputGob = document.querySelector('.popup__input_type-specification');
+let inputPlace = document.querySelector('.popup__input_type-place');
+let inputLink = document.querySelector('.popup__input_type-link');
+
+
+
+const profileSpecification = document.querySelector('.profile__specification');
+const profileName = document.querySelector('.profile__title');
+
+//объявляем функции открытия и закрытия попапов
+const openForm = (popup) => {  
+  popup.classList.add('popup_opened');
+}
+const closeForm = (popup) => {
   popup.classList.remove('popup_opened');
 }
-closePopupButton.addEventListener('click', formClose);
-function formSubmitHandler (evt) {
-  evt.preventDefault(); 
-  profileName.textContent = nameInput.value; 
-  profileSpecification.textContent = jobInput.value;
-  formClose();
+const fillProfileForm = () => { //заполняем поля профайл-попапа данными активного пользователя
+  inputName.value = profileName.textContent;
+  inputGob.value = profileSpecification.textContent;
 }
-formPopup.addEventListener('submit', formSubmitHandler);
+
+//добавляем слушатель кнопке "редактировать профиль" для открытия профайл-попапа
+buttonOpenProfile.addEventListener('click', () => {
+  openForm(profilePopup);
+  fillProfileForm();
+});
+
+//закрытие попапов по нажатию на крестик
+buttonCloseProfile.addEventListener('click', () => {
+  closeForm(profilePopup); 
+}); 
+buttonClosePlace.addEventListener('click', () => {
+  placePopup.querySelector('.popup__form_place').reset(); //очистка формы place при закрытии по крестику
+  closeForm(placePopup); 
+}); 
+
+
+
+function profileSubmitHandler (evt) { //ф-я сабмитит форму редактирования профайла
+  evt.preventDefault(); 
+  profileName.textContent = inputName.value; 
+  profileSpecification.textContent = inputGob.value;
+  closeForm(profilePopup);
+}
+
+submitProfile.addEventListener('submit', profileSubmitHandler);
+
+buttonOpenPlacePopup = document.querySelector('.profile__adding-button');
+buttonOpenPlacePopup.addEventListener('click', () => {
+  openForm(placePopup);
+});
+
+function placeSubmitHandler (evt) { //ф-я сабмитит форму редактирования профайла
+  evt.preventDefault(); 
+  const newCard = {};
+  newCard.name = inputPlace.value; 
+  newCard.link = inputLink.value;
+  postingCard(newCard);
+  this.reset();
+  closeForm(placePopup);
+};
+
+
+submitNewPlace.addEventListener('submit', placeSubmitHandler );
