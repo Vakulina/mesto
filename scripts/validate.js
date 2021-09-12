3b5742c957d770b1235d04547e1cae7b2bc31187
Vakulina <viktoria.ganina@yandex.ru>
2021-09-12 12:14:49 +0300

    feat: add main logic of validation

---
+++ scripts/validate.js
@@ -0,0 +1,58 @@
+const config = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible',
+}; 
+const showInputError = (formElement, inputElement, errorClass, inputErrorClass) => {
+  const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
+  inputElement.classList.add(errorClass);
+  errorElement.textContent =  inputElement.validationMessage;
+  errorElement.classList.add(inputErrorClass);
+  console.log('показать ошибку');
+};
+const hideInputError = (formElement, inputElement, errorClass, inputErrorClass) => {
+  const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
+  inputElement.classList.remove(errorClass);
+  errorElement.classList.remove(inputErrorClass);
+  errorElement.textContent = '';
+  console.log(' скрыть ошибку');
+};
+const checkInputsValidity = (formElement, inputElement, errorClass, inputErrorClass) => {
+  console.log(inputElement.validity.valid);
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, errorClass, inputErrorClass);
+  } else {
+    hideInputError(formElement, inputElement, errorClass, inputErrorClass);
+  }
+}
+//const toogleButtonsState
+const setInputsListeners = (formElement, inputSelector, errorClass, inputErrorClass)=>{
+  const listInputs= Array.from(formElement.querySelectorAll(inputSelector));
+  listInputs.forEach(inputElement =>{
+    inputElement.addEventListener('input', e => {
+    e.preventDefault();
+    checkInputsValidity(formElement, inputElement, errorClass, inputErrorClass);
+
+    });
+  })
+  }
+
+const setFormsListeners = (formElement, inputSelector, errorClass, inputErrorClass)=>{
+  formElement.addEventListener('submit', e=>{
+    e.preventDefault();
+    
+  })
+  setInputsListeners(formElement, inputSelector, errorClass, inputErrorClass);
+  
+}
+const enableValidate = (config) => {
+  const listForms = document.querySelectorAll(config.formSelector);
+  listForms.forEach((formElement)=>{
+    setFormsListeners(formElement, config.inputSelector, config.errorClass, config.inputErrorClas);
+       
+  });
+}
+enableValidate(config);
